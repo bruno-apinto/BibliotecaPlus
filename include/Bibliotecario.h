1586f09dab74cecf11ef4e5a39f67153f9878ace
Samira Mansur <148010885+samimansur@users.noreply.github.com>
2023-10-23 00:30:30 -0300

    Update Bibliotecario.h

--- include/Bibliotecario.h
+++ include/Bibliotecario.h
@@ -19,17 +19,17 @@ class Bibliotecario {
 public:
     Bibliotecario(std::string nome, std::string telefone, std::string login, std::string senha, std::string email, std::string matricula);
 
-    std::vector<Livro> ProcurarLivrosPorTitulo(Alunos& aluno, std::string titulo);
-    std::vector<Livro> ProcurarLivrosPorAutor(Alunos& aluno, std::string autor);
-    std::vector<Livro> ProcurarLivrosPorEditora(Alunos& aluno, std::string editora);
-    void EditarAcervoAdicionarLivro(const Livro& livro);
-    void EditarAcervoRemoverLivro(const std::string& titulo);
-    void EditarAcervoEditarLivro(const std::string& titulo, const std::string& novoTitulo, const std::string& novoAutor, const std::string& novaEditora, const std::string& novosDetalhes);
-    void EditarCadastrosAdicionarPessoa(const Alunos& aluno);
-    void EditarCadastrosRemoverPessoa(const std::string& matricula);
-    void EditarCadastrosEditarPessoa(const std::string& matricula, const std::string& novoNome, const std::string& novaSenha, const std::string& novoEmail);
-    void EmprestarLivro(const Alunos& aluno, const Livro& livro);
-    void ReceberLivro(const Alunos& aluno, const Livro& livro);
+    std::vector<Livro> ProcurarLivrosPorTitulo(Alunos& aluno, std::string titulo); // lista que retorna a busca de livros por título
+    std::vector<Livro> ProcurarLivrosPorAutor(Alunos& aluno, std::string autor); // lista que retorna a busca de livros por autor
+    std::vector<Livro> ProcurarLivrosPorEditora(Alunos& aluno, std::string editora); // lista que retorna a busca de livros por editora
+    void EditarAcervoAdicionarLivro(const Livro& livro); // função que permite adicionar livros ao acervo
+    void EditarAcervoRemoverLivro(const std::string& titulo); // função que permite remover livros do acervo
+    void EditarAcervoEditarLivro(const std::string& titulo, const std::string& novoTitulo, const std::string& novoAutor, const std::string& novaEditora, const std::string& novosDetalhes); // função que permite editar as informações de livros no acervo
+    void EditarCadastrosAdicionarPessoa(const Alunos& aluno); // função que permite adicionar cadastro de novas pessoas
+    void EditarCadastrosRemoverPessoa(const std::string& matricula); // função que permite remover cadastros
+    void EditarCadastrosEditarPessoa(const std::string& matricula, const std::string& novoNome, const std::string& novaSenha, const std::string& novoEmail); // função que permite editar as informações do cadastro de usuários
+    void EmprestarLivro(const Alunos& aluno, const Livro& livro); // função que atualiza a situação do livro para emprestado
+    void ReceberLivro(const Alunos& aluno, const Livro& livro); // função que atualiza a situação do livro para recebido 
     
     /**
      * @brief Set the Situacao object
@@ -39,7 +39,7 @@ public:
     void setSituacao(SituacaoEmprestimo novaSituacao);
 
 private:
-    struct Pessoa {
+    struct Pessoa { // struct dos dados do usuário
         std::string nome;
         std::string telefone;
         std::string login;
@@ -48,7 +48,7 @@ private:
         std::string matricula;
     };
 
-    std::vector<Livro> acervo;
-    std::vector<Alunos> cadastros;
+    std::vector<Livro> acervo; // lista com os livros do acervo
+    std::vector<Alunos> cadastros; // lista com os alunos cadastrados
 };
 #endif
