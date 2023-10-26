b409c325fdba35b7ca9d9517ef99deac4a4d071f
Beatriz <bpissioni@gmail.com>
2023-10-26 18:26:30 -0300

    acervoalunos

--- include/AcervoLivros.h
+++ include/AcervoLivros.h
@@ -24,9 +24,6 @@ public:
     void adicionarLivro(const Livro& livro); // função que permite adicionar livros ao AcervoLivros
     void removerLivro(const std::string& titulo); // função que permite remover livros do AcervoLivros
     void editarLivro(const std::string& titulo, const std::string& novoTitulo, const std::string& novoAutor, const std::string& novaEditora, const std::string& novosDetalhes); // função que permite editar as informações de livros no AcervoLivros
-    void adicionarPessoa(const Alunos& aluno); // função que permite adicionar cadastro de novas pessoas
-    void removerPessoa(const std::string& matricula); // função que permite remover cadastros
-    void editarPessoa(const std::string& matricula, const std::string& novoNome, const std::string& novaSenha, const std::string& novoEmail); // função que permite editar as informações do cadastro de usuários
     void emprestarLivro(const Alunos& aluno, const Livro& livro); // função que atualiza a situação do livro para emprestado
     void receberLivro(const Alunos& aluno, const Livro& livro); // função que atualiza a situação do livro para recebido 
     
