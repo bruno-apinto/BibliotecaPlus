db8995179517de217d3cbaa7b6072a4e2475fd8c
Beatriz <bpissioni@gmail.com>
2023-10-26 17:56:02 -0300

    acervoalunos

--- include/AcervoLivros.h
+++ include/AcervoLivros.h
@@ -1,5 +1,5 @@
-#ifndef ACERVO_H
-#define ACERVO_H
+#ifndef ACERVOALUNOS_H
+#define ACERVOALUNOS_H
 
 #include "Aluno.h"
 #include "Livro.h"
@@ -16,14 +16,14 @@ struct Pessoa {
         std::string matricula;
     };
 
-class Acervo {
+class AcervoLivros {
 public:
-    Acervo(std::string nome, std::string telefone, std::string login, std::string senha, std::string email, std::string matricula);
+    AcervoLivros(std::string nome, std::string telefone, std::string login, std::string senha, std::string email, std::string matricula);
 
     std::vector<Livro> procurarLivros(std::string parametro, std::string item, std::vector<Livro> livrosEncontrados);
-    void adicionarLivro(const Livro& livro); // função que permite adicionar livros ao acervo
-    void removerLivro(const std::string& titulo); // função que permite remover livros do acervo
-    void editarLivro(const std::string& titulo, const std::string& novoTitulo, const std::string& novoAutor, const std::string& novaEditora, const std::string& novosDetalhes); // função que permite editar as informações de livros no acervo
+    void adicionarLivro(const Livro& livro); // função que permite adicionar livros ao AcervoLivros
+    void removerLivro(const std::string& titulo); // função que permite remover livros do AcervoLivros
+    void editarLivro(const std::string& titulo, const std::string& novoTitulo, const std::string& novoAutor, const std::string& novaEditora, const std::string& novosDetalhes); // função que permite editar as informações de livros no AcervoLivros
     void adicionarPessoa(const Alunos& aluno); // função que permite adicionar cadastro de novas pessoas
     void removerPessoa(const std::string& matricula); // função que permite remover cadastros
     void editarPessoa(const std::string& matricula, const std::string& novoNome, const std::string& novaSenha, const std::string& novoEmail); // função que permite editar as informações do cadastro de usuários
@@ -47,7 +47,7 @@ private:
         std::string matricula;
     };
 
-    std::vector<Livro> acervo; // lista com os livros do acervo
+    std::vector<Livro> AcervoLivros; // lista com os livros do AcervoLivros
     std::vector<Alunos> cadastros; // lista com os alunos cadastrados
 };
 #endif
