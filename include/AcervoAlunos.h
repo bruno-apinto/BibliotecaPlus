3aada291ecf95dac7251774952616180b2e04d35
samimansur <samira.mansurc@gmail.com>
2023-10-27 11:13:52 -0300

    include

--- include/AcervoAlunos.h
+++ include/AcervoAlunos.h
@@ -6,6 +6,17 @@
 #include <string>
 #include <vector>
 
-void editarPessoa(const std::string& matricula, const std::string& novoNome, const std::string& novaSenha, const std::string& novoEmail); // função que permite editar as informações do cadastro de usuários
-void removerPessoa(const std::string& matricula); // função que permite remover cadastros
-void adicionarPessoa(const Alunos& aluno); // função que permite adicionar cadastro de novas pessoas
+class AcervoAlunos {
+
+public:
+
+void editarAluno(const std::string& matricula, const std::string& novoNome, const std::string& novaSenha, const std::string& novoEmail); // função que permite editar as informações do cadastro de usuários
+void removerAluno(const std::string& matricula); // função que permite remover cadastros
+void adicionarAluno(const Alunos& aluno); // função que permite adicionar cadastro de novas pessoas
+
+private:
+
+std::vector<Alunos> cadastros; // lista com os alunos cadastrados
+};
+
+#endif
