565557d0778488fe49d25c6a9b7f583bc9a123e0
samimansur <samira.mansurc@gmail.com>
2023-10-27 09:50:28 -0300

    Commit das alterações locais

--- include/AcervoLivros.h
+++ include/AcervoLivros.h
@@ -7,15 +7,6 @@
 #include <string>
 #include <vector>
 
-struct Pessoa {
-        std::string nome;
-        std::string telefone;
-        std::string login;
-        std::string senha;
-        std::string email;
-        std::string matricula;
-    };
-
 class AcervoLivros {
 public:
     AcervoLivros(std::string nome, std::string telefone, std::string login, std::string senha, std::string email, std::string matricula);
