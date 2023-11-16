602a0fd29b8ebdf3a8a93bc1eaade9708b99188d
brunopinto <brunoapinto2004@gmail.com>
2023-11-16 18:04:14 -0300

    procurar livros overloading

--- src/AcervoLivros.cpp
+++ src/AcervoLivros.cpp
@@ -15,27 +15,38 @@ AcervoLivros::AcervoLivros() {}
 std::vector<Livro> AcervoLivros::procurarLivros (std::string categoria, std::string item) {
 
     string linha;
-    std::ifstream leituraPadrao ("arquivos/livros/livros.txt");
-
+    std::ifstream leituraPadrao ("arquivos/Livros/livros.txt");
+    std::ifstream leituraStatus ("arquivos/Livros/status.txt");
+    
     int i = 1;
-    string caracteristicas[7];
+    string caracteristicas[8];
     vector<Livro> lista;
     
     
     while (!leituraPadrao.eof()) {
-        getline (leituraPadrao, linha, "|");
-        caracteristicas[i%8] = linha;
+       
+        if(i%8 != 7){ 
+        getline (leituraPadrao, caracteristicas[i%8], '|');}
+
+        else {
+            for (auto it = lista.begin(); it != lista.end(); it++){
+                for (int a = 0; a<3; a++){
+                    getline(leituraStatus, caracteristicas[7], '|');
+                }
+            }
+        }
 
         if (i%8 == 0) {
             lista.push_back(Livro(caracteristicas[0], caracteristicas[1], caracteristicas[2],
                                     caracteristicas[3], caracteristicas[4], caracteristicas[5], 
-                                    caracteristicas[6], " "));
+                                    caracteristicas[6], caracteristicas[7]));
         }
         i++;
         }
 
     
     leituraPadrao.close();
+    leituraStatus.close();
 
     lista = AcervoLivros::procurarLivros(categoria, item, lista);
     return lista;
